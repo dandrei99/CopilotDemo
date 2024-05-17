b8e618f24fe02cae521ef4a4da9286ab134189ad
Dan Onofrei <dan.onofrei@ibm.com>
2024-05-17 16:58:54 +0300

    sum 2 nr

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        int sum = add(2, 3);
+        System.out.println(sum);
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
